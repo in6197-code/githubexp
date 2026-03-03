bd77ee49b1454a750dfa2fe0e98f977573d2ae3e
in6197-code <in6197@srmist.edu.in>
2026-03-03 09:55:13 +0530

    first commit

---
+++ usecase1.java
@@ -0,0 +1,23 @@
+/**
+ * OOPSBannerApp
+ * 
+ * This application prints the text "OOPS" to the console.
+ * It demonstrates the basic structure of a Java program.
+ * 
+ * @author RITIK
+ * @version 1.0
+ */
+
+public class usecase1 {
+
+    /**
+     * Main method - Entry point of the application
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+
+        // Print OOPS to console
+        System.out.println("OOPS");
+
+    }
+}
